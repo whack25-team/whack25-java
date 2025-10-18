0f2e1826ea15a6dc1c333f758f9d67f9168dda51
Infinity <60041234+infinitelyjames@users.noreply.github.com>
2025-10-18 20:48:38 +0100

    GraphNode - added dijkstra

--- core/src/main/java/livegraph/GraphNode.java
+++ core/src/main/java/livegraph/GraphNode.java
@@ -1,13 +1,16 @@
 package livegraph;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class GraphNode<R,N> {
     private final N nodeId;
     private int x;
     private int y;
     private NodeType tileType;
-    private List<ConnectedNode<R, N>> neighbours;
+    private List<ConnectedNode<R, N>> neighbours; // adjacent OUTGOING nodes. this class does not store incoming edges.
     private List<RobotMovement<R, N>> occupiers;
 
     public GraphNode(N nodeId, int x, int y, NodeType tileType, List<ConnectedNode<R,N>> neighbours, List<RobotMovement<R,N>> occupiers) {
@@ -32,5 +35,80 @@ public class GraphNode<R,N> {
         }
     }
 
+    // Gets the next nodeid on the shortest path to the destination
+    public GraphNode<R,N> getNextNodeOnPath(N destinationNodeId) { // gets the next node on the shortest path to the destination
+        PriorityQueue<PathNode<R, N>> pathsToExplore = new PriorityQueue<>((path1, path2) -> Integer.compare(path1.cumulativeWeight, path2.cumulativeWeight) );
 
+        pathsToExplore.add(new PathNode<>(new ArrayList<>(List.of(this)), 0));
+
+        while (!pathsToExplore.isEmpty()) {
+            PathNode<R,N> currentPath = pathsToExplore.poll();
+
+            GraphNode<R,N> currentNode = currentPath.path.get(currentPath.path.size() - 1);
+
+            // If we reached the destination, return the next node in the path
+            if (currentNode.nodeId.equals(destinationNodeId)) {
+                // System.out.println("Length of path found: " + currentPath.path.size() + " with total weight: " + currentPath.cumulativeWeight);
+//                for (GraphNode<R,N> node : currentPath.path) {
+//                    System.out.print(node.nodeId + " -> ");
+//                }
+//                System.out.println("(END)");
+                return currentPath.path.get(1); // Return the next node after the start node
+            }
+
+            // Explore neighbours
+            for (ConnectedNode<R,N> neighbour : currentNode.getNeighbours()) {
+                if (currentPath.path.stream().noneMatch(node -> node.nodeId.equals(neighbour.node.nodeId))) { // Avoid cycles
+                    PathNode<R,N> newPath = currentPath.addStep(neighbour.node, neighbour.edgeWeight);
+                    pathsToExplore.add(newPath);
+                }
+            }
+        }
+
+        return null; // No path found
+
+    }
+
+    // Getters
+    public List<ConnectedNode<R,N>> getNeighbours() {
+        return this.neighbours;
+    }
+
+    // Setters
+    public void addNeighbour(ConnectedNode<R,N> neighbour) {
+        this.neighbours.add(neighbour);
+    }
+
+    public void setNeighbours(List<ConnectedNode<R,N>> neighbours) {
+        this.neighbours = neighbours;
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        return "GraphNode{" +
+                "nodeId=" + nodeId +
+                ", x=" + x +
+                ", y=" + y +
+                ", tileType=" + tileType +
+                '}';
+    }
+
+
+}
+
+class PathNode<R, N> {
+    public final List<GraphNode<R,N>> path;
+    public final int cumulativeWeight;
+
+    public PathNode(List<GraphNode<R,N>> path, int cumulativeWeight) {
+        this.path = path;
+        this.cumulativeWeight = cumulativeWeight;
+    }
+
+    public PathNode<R,N> addStep(GraphNode<R,N> node, int weight) {
+        List<GraphNode<R,N>> newPath = new ArrayList<>(List.copyOf(path));
+        newPath.add(node);
+        return new PathNode<>(newPath, cumulativeWeight + weight);
+    }
 }
