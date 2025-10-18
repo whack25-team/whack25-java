99b5258e2a53b914a428c59e0925b77df7d65734
Infinity <60041234+infinitelyjames@users.noreply.github.com>
2025-10-18 13:14:36 +0100

    Main.java - shows basic texture

--- core/src/main/java/io/github/whack25/Main.java
+++ core/src/main/java/io/github/whack25/Main.java
@@ -1,14 +1,25 @@
 package io.github.whack25;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    FitViewport viewport;
+    Texture backgroundTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
 
     @Override
     public void create() {
@@ -18,13 +29,45 @@ public class Main extends ApplicationAdapter {
 
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        viewport = new FitViewport(8,5);
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        input();
+        logic();
+        draw();
+//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+//        batch.begin();
+//        batch.draw(image, 140, 210);
+//        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
-        batch.draw(image, 140, 210);
+        // Draw in world units
+        batch.draw(bucketTexture, 0,0, 1, 1);
         batch.end();
     }
 
