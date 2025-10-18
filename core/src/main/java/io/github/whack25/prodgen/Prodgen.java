70efefc4d0544f559d40931bc07e164ba33e4c6d
samwrourke <sam.w.rourke@icloud.com>
2025-10-18 16:34:48 +0100

    Utility methods
    Added way to add rectangles to worldarrays,
    Added method to print the array for debugging

--- core/src/main/java/io/github/whack25/prodgen/Prodgen.java
+++ core/src/main/java/io/github/whack25/prodgen/Prodgen.java
@@ -13,7 +13,37 @@ public class Prodgen {
 		return worldarray;
 	}
 
-	//public static void addrect
+	public static void addrect(WorldElement[][] worldarray,
+			int x,int y,int w,int h,
+			boolean solid,double windx,double windy) {
+
+		for (int xi = x; xi < x + w; xi++) {
+			if (xi >= 0 && xi < worldarray.length) {
+				for (int yi = y; yi < yi + h; yi++) {
+					if ((yi >= 0 && yi < worldarray[0].length) && worldarray[xi][yi].solid) {
+						worldarray[xi][yi].solid = solid;
+						worldarray[xi][yi].windx = windx;
+						worldarray[xi][yi].windy = windy;
+					}
+				}
+			}
+		}
+	}
+
+	public static void addrandomrect(WorldElement[][] worldarray,
+			int numrects,int minx,int rangex,int miny,int rangey,int minw,int rangew,int minh,int rangeh,
+			double solidchance,	// If Math.random() larger than this, is soild
+			double minwindx,double rangewindx,double minwindy,double rangewindy) {
+		for (int i = 0; i < numrects; i++) {
+			addrect(worldarray,minx + (int) ((float) rangex * Math.random()),
+					miny + (int) ((float) rangey * Math.random()),
+					minw + (int) ((float) rangew * Math.random()),
+					minh + (int) ((float) rangeh * Math.random()),
+					Math.random() > solidchance,
+					minwindx + (int) ((float) rangewindx * Math.random()),
+					minwindy + (int) ((float) rangewindy * Math.random()));
+		}
+	}
 
 	public static boolean colliderectwithworldarray(WorldElement[][] worldarray,
 			int x,int y,int w,int h) {
@@ -27,5 +57,14 @@ public class Prodgen {
 			}
 		}
 		return false;
+	}
+
+	public static void printsolid(WorldElement[][] worldarray) {
+		for (int x = 0; x < worldarray.length; x++) {
+			for (int y = 0; y < worldarray[0].length; y++) {
+				System.out.print(worldarray[x][y].solid ? "S" : " ");
+			}
+			System.out.print("\n");
+		}
 	}
 }
