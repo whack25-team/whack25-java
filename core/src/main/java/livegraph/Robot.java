11e71c5ef714de5dae5caf2c075e6f5ab42c679f
Infinity <60041234+infinitelyjames@users.noreply.github.com>
2025-10-18 21:06:17 +0100

    Robot - added functions

--- core/src/main/java/livegraph/Robot.java
+++ core/src/main/java/livegraph/Robot.java
@@ -1,6 +1,11 @@
 package livegraph;
 
 public class Robot<T, N> {
-    T robotID;
-    N destinationNodeId;
+    final public T robotID;
+    final public N destinationNodeId;
+
+    public Robot(T robotID, N destinationNodeId) {
+        this.robotID = robotID;
+        this.destinationNodeId = destinationNodeId;
+    }
 }
